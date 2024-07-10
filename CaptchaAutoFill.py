71e1260ba209dae4be0044012706c167ff4ee7c0
tunanm <tuan.ptit.5899@gmail.com>
2024-07-10 22:02:33 +0700

    read newest email, screen capture, click captcha

--- CaptchaAutoFill.py
+++ CaptchaAutoFill.py
@@ -2,21 +2,62 @@ import os
 import ssl
 import smtplib
 import datetime
+import re
+import pyautogui as gui
 from email.message import EmailMessage
-from simplegmail
+from simplegmail import Gmail
+from simplegmail.query import construct_query
 # Set up the SMTP server and login details
+captcha = ''
+captcha_file_path = 'Resources\Captcha\captcha.jpg'
 
 def screen_captcha_capture():
-    return ''
+    screen_shot = gui.screenshot(region=(0, 0, 850, 520))
+    screen_shot.save(captcha_file_path)
 def read_email_reply():
-    return ''
-def fill_in_captcha():
-    return ''
+    global captcha
+    gmail = Gmail()
+    query_params = {
+        'newer_than':(1,"day")
+    }
+    messages = gmail.get_unread_inbox(query=construct_query(query_params))
+    if "Captcha" in messages[0].subject:
+        match = re.findall(r"\D(\d{5})\D", " "+messages[0].plain+" ")
+        if match:
+            captcha = match
+    return captcha[0]
+
+def fill_in_captcha(numbers):
+    ind = 0
+    while ind <= 5:
+        gui.click()
+        ind += 1
+    for number in numbers:
+        if number == '0':
+            gui.click()
+        if number == '1':
+            gui.click()
+        if number == '2':
+            gui.click()
+        if number == '3':
+            gui.click()
+        if number == '4':
+            gui.click()
+        if number == '5':
+            gui.click()
+        if number == '6':
+            gui.click()
+        if number == '7':
+            gui.click()
+        if number == '8':
+            gui.click()
+        if number == '9':
+            gui.click()
 def send_captcha_email():
     sender_email = 'gopetcaptchanotification@gmail.com'
     password = os.environ.get('EMAIL_PASSWORD')
     # Create the email headers and body
-    receive_email = 'tuanquangtrungtuky5899@gmail.com'
+    receive_email = 'saometvay5899@gmail.com'
     subject = f'Captcha {datetime.datetime.now()}'
     body = ''
     msg = EmailMessage()
@@ -32,5 +73,3 @@ def send_captcha_email():
     with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
         smtp.login(sender_email,password)
         smtp.sendmail(sender_email,receive_email,msg.as_string())
-
-send_captcha_email()
